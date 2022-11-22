ccccdc141444501eb4e98f25d80b9071d87b7453
Wan Chang Lai <wanchang.lai@live.com>
2022-11-22 21:24:30 +0800

    Create a Store

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import {createStore} from 'redux';
+
+//
+const counterReducer = (state = {counter:0} ,action) =>{
+    if (action.type === 'increment'){
+        return {
+            counter : state.counter + 1,
+        };
+    }
+
+    if (action.type === 'decrement'){
+        return {
+            counter : state.counter - 1,
+        };
+    }
+
+    return state;
+
+};
+
+//pass through the reducer function to the store
+const store = createStore(counterReducer);
+
+//connect App to the store
+export default store;
