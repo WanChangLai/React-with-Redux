4f08903f062970b33293a3e87a9a433aaff98d8c
Wan Chang Lai <wanchang.lai@live.com>
2022-11-23 15:03:54 +0800

    Add State Slice from Redux Toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,34 @@
 import {createStore} from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = { counter:0, showCounter: true }
+
+//take object as an input 
+createSlice({
+  name: 'counter',
+  initialState,
+  reducers:{
+    //method here
+    increment(state){
+        //mutate the state is allowed here
+        state.counter++;
+    },
+    derrement(state){
+        state.counter--;
+    },
+    increase(state,action){
+        state.counter = state.action + action.amount;
+    },
+    toggle(state){
+        state.showCounter = !state.showCounter
+    }
+  }
+});
 
 const counterReducer = (state = initialState, action) =>{
     if (action.type === 'increment'){
         return {
+            //always overwrite. without mention here, always false
             counter : state.counter + 1,
             showCounter: state.showCounter
         };
